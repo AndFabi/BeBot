6449c524057064734e8cfa2d629e6a9a08406eea
Fabi <fabiandreacola@gmail.com>
2015-09-28 16:57:20 +0200

    init

---
+++ app/assets/javascripts/button_anim.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+
+
+   $(".button-fill").hover(function () {
+      $(this).children(".button-inside").addClass('full');
+  }, function() {
+    $(this).children(".button-inside").removeClass('full');
+  });
+});
---
+++ app/assets/javascripts/scroll_anim.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function() {
+
+
+
+    // // navbar transition jQuery script
+    $(window).scroll(function(e){
+      if ($(this).scrollTop() > 300) {
+        $(".navbar-wagon-fixed").css("margin-top", "-15px");
+      }
+      else {
+        $(".navbar-wagon-fixed").css("margin-top", "0px");
+      }
+    });
+
+
+
+
+
+
+    //Scrool to section
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 800);
+        return false;
+      }
+    }
+  });
+
+  //scroll slide animation
+  window.sr = new scrollReveal();
+
+
+
+});
